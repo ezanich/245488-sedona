6ce8c9505dcc12d9a5d27f5cd84526b26c8229f4
RinAlexx <43240047+RinAlexx@users.noreply.github.com>
2018-12-18 14:12:56 +0300

    Подогнала отступы под PerfectPixel

--- js/script.js
+++ js/script.js
@@ -1,9 +1,7 @@
 var link = document.querySelector(".button-drop");
-			var popup = document.querySelector(".reservation");
+var popup = document.querySelector(".reservation");
 
-			link.addEventListener("click", function(evt) {
-				evt.preventDefault();
-				popup.classList.toggle("reservation-show");
-
-
-			})
+link.addEventListener("click", function(evt) {
+	evt.preventDefault();
+	popup.classList.toggle("reservation-show");
+});
